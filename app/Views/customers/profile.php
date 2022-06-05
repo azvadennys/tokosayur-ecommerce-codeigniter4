02e1877361acb15e22fc58d9ab0a0797b87f164c
Azvadennys Vasiguhamiaz <azvadenis@gmail.com>
2022-06-05 14:05:46 +0700

    fix bug

--- app/Views/customers/profile.php
+++ app/Views/customers/profile.php
@@ -5,9 +5,9 @@
   <div class="container">
     <div class="row no-gutters slider-text align-items-center justify-content-center">
       <div class="col-md-9 ftco-animate text-center">
-        <p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home</a></span> <span>Products</span>
+        <p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home</a></span> <span>Profile</span>
         </p>
-        <h1 class="mb-0 bread">Pembayaran Saya</h1>
+        <h1 class="mb-0 bread">Profile Saya</h1>
       </div>
     </div>
   </div>
